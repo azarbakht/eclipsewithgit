16f903a69b37f0baf37935cc209d0d7a32d2f86f
Emerson Amirhosein Azarbakht <azarbaam@eecs.oregonstate.edu>
2015-05-18 20:37:29 -0700

    dialogbox added

--- EclipseWithGit/src/eclipseWithGit/main.java
+++ EclipseWithGit/src/eclipseWithGit/main.java
@@ -1,9 +1,12 @@
 package eclipseWithGit;
 
+import javax.swing.JOptionPane;
+
 public class main
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Salaam jigar");		
+		System.out.println("Salaam jigar");
+		JOptionPane.showMessageDialog(null, "in ham ye message too Dialog Box. OK?");
 	}
 }
